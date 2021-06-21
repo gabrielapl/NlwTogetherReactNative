c88acbe20e72bc10e3e438feb6a41d2590120871
gabrielapl <gabrielp.a123456789@gmai.com>
2021-06-21 11:09:01 -0300

    day 01 finished

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors:{
+    background: "#0E1647",
+    heading: "#DDE3F0",
+    body: "#ABB1CC",
+
+    primary: "#E51C44",
+    line: "#991F36",
+
+    secondary: "#495BCC",
+  }
+}
